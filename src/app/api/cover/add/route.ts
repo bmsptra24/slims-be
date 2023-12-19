870de62fadd6eccb93116d6cf9aa2d10a89cee9f
Bima Saputra <109858732+bmsptra24@users.noreply.github.com>
2023-12-19 08:00:12 +0700

    Delete route.ts

--- src/app/api/cover/add/route.ts
+++
@@ -1,7 +0,0 @@
-// import { NextRequest, NextResponse } from 'next/server'
-
-// export async function GET(request: NextRequest) {
-
-//       return Response.json(response, { status: 200 })
-
-// }
